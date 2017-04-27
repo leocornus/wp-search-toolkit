687e1ea3760f400f445667bb60251ab06ddcbc54
Sean Chen <Sean.Chen@leocorn.com>
2017-04-27 13:06:42 -0400

    introduce the admin/index.php and code re-organize

---
+++ admin/index.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * manage site admin dashboard settings pages.
+ */
+
+// register the deactivation script.
+register_deactivation_hook($search_toolkit_file, 
+                           'deactivate_search_toolkit');
+
+/**
+ * the deactivation script will simplely remove all options for now.
+ */
+function deactivate_search_toolkit() {
+
+    delete_option('livesearch_input_id');
+    delete_option('livesearch_filter_options');
+}
+
+// adding the dashboard page to manage
+add_action('admin_menu', 'search_toolkit_admin_init');
+
+function search_toolkit_admin_init() {
+
+    $main_page = SEARCH_TOOLKIT_BASE_PATH . '/admin/livesearch.php';
+    $advanced = SEARCH_TOOLKIT_BASE_PATH . '/admin/advancedsearch.php';
+
+    add_menu_page('Search Toolkit', 'Search Toolkit',
+                  'manage_options',
+                  $main_page,
+                  '');
+
+    // the general settings page.
+    add_submenu_page($main_page,
+                     'Search Toolkit LiveSearch Settings',
+                     'LiveSearch Settings',
+                     'manage_options',
+                     $main_page);
+
+//    add_submenu_page($main_page,
+//                     'Search Toolkit Advanced Search Settings',
+//                     'Advanced Search',
+//                     'manage_options',
+//                     $advanced);
+}
--- search-toolkit.php
+++ search-toolkit.php
@@ -1,10 +1,12 @@
 <?php
 /*
 Plugin Name: Search Toolkit
+Plugin URI: http://www.github.com/leocornus/wp-search-toolkit
 Description: The WordPress plugin to host useful tools for search.
 Version: 0.1
 Author: Sean Chen 
 Author URI: http://github.com/seanchen
+License: GPLv2
 */
 
 // the symlink safe way for plugin path.
@@ -16,47 +18,9 @@ define('SEARCH_TOOLKIT_PATH',
        WP_PLUGIN_DIR . '/' . SEARCH_TOOLKIT_BASE_PATH);
 
 // load the admin page index file, which will load all admin page.
-//require_once(SEARCH_TOOLKIT_PATH . '/admin/index.php');
+require_once(SEARCH_TOOLKIT_PATH . '/admin/index.php');
 require_once(SEARCH_TOOLKIT_PATH . '/functions.php');
 
-// register the deactivation script.
-register_deactivation_hook($search_toolkit_file, 
-                           'deactivate_search_toolkit');
-
-/**
- * the deactivation script will simplely remove all options for now.
- */
-function deactivate_search_toolkit() {
-
-    delete_option('livesearch_input_id');
-    delete_option('livesearch_filter_options');
-}
-
-// adding the dashboard page to manage
-add_action('admin_menu', 'search_toolkit_admin_init');
-function search_toolkit_admin_init() {
-
-    $main_page = SEARCH_TOOLKIT_BASE_PATH . '/admin/livesearch.php';
-    $advanced = SEARCH_TOOLKIT_BASE_PATH . '/admin/advancedsearch.php';
-
-    add_menu_page('Search Toolkit', 'Search Toolkit',
-                  'manage_options',
-                  $main_page,
-                  '');
-
-    // the general settings page.
-    add_submenu_page($main_page,
-                     'Search Toolkit LiveSearch Settings',
-                     'LiveSearch Settings',
-                     'manage_options',
-                     $main_page);
-
-//    add_submenu_page($main_page,
-//                     'Search Toolkit Advanced Search Settings',
-//                     'Advanced Search',
-//                     'manage_options',
-//                     $advanced);
-}
 
 /**
  * a simple filter hook function to demostrate the filter format.
